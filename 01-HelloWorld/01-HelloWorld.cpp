5b1c5363fa74f60aaa6ffce08c103ce0e24bcb6a
Kieran Everett <kieraneverett03@gmail.com>
2021-07-05 22:10:30 +0100

    Created a Hello World program

---
+++ 01-HelloWorld/01-HelloWorld.cpp
@@ -0,0 +1,14 @@
+// including the input output stuff
+#include <iostream>
+
+// an option to reduce code a little is to use this line but it is not recommended because its really big
+//using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!";
+    //cout << "Hello World!"; // this line of code is for when you use the std namespace, you don't need to find it in the std namespace as its part of the main namespace
+
+    // returing 0 at an int main is good practice
+    return 0;
+}
