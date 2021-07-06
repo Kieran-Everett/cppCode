54d290124b95d6c90cc60537cd8f6b45948c3bbc
Kieran Everett <kieraneverett03@gmail.com>
2021-07-06 22:23:10 +0100

    Created 05-UserInput

---
+++ 05-UserInput/05-UserInput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int x;
+    std::cout << "Number: ";
+    std::cin >> x;
+    std::cout << "That number is " << x << std::endl;
+    std::string e = "hey"; // what the frick is this
+    std::cout << e << std::endl;
+
+    //                  i   dont think i    want ot do   C++ any more
+}
