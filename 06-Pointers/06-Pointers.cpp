2a3eca7e13c249f4c55d687f04a1f5c4c7bdcc88
Kieran Everett <kieraneverett03@gmail.com>
2021-07-07 14:00:40 +0100

    Update 06-Pointers.cpp

--- 06-Pointers/06-Pointers.cpp
+++ 06-Pointers/06-Pointers.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main()
+void withNumbers()
 {
     int a = 10;
     cout << a << endl;
@@ -17,7 +17,7 @@ int main()
     int someArray[10] = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
 
     int* pLocation6 = &someArray[6];
-    
+
     // either of these work because index 0 is the start of the data in RAM of the array
     int* pLocation0 = &someArray[0]; // this one is a bit clearer though
     //int* pLocation0 = someArray;
@@ -29,7 +29,37 @@ int main()
     for (int i = 0; i < 10; i++)
     {
         cout << someArray[i] << endl; // outputs the array
-        cout << someArray + i << endl; // outputs the pointer for each location of the array
-        cout << someArray + i << " = " << *(someArray + i) << endl; // gets the value through the pointer
+        //cout << someArray + i << endl; // outputs the pointer for each location of the array
+        //cout << someArray + i << " = " << *(someArray + i) << endl; // gets the value through the pointer
+
+        // this example increments the pointer itself
+        cout << pLocation0 << " = " << *pLocation0 << endl;
+        pLocation0++;
     }
+}
+
+void withStrings()
+{
+    // now with a string
+    char someString[] = "Hello!";
+
+    char* pSomeString = someString;
+
+    cout << "pSomeString = " << pSomeString << endl;
+
+    char* pLocation3 = &someString[3];
+    char* pLocation0 = &someString[0];
+
+    cout << "pLocation3 = " << (int)pLocation3 << endl;
+    cout << "pLocation0 = " << (int)pLocation0 << endl;
+    cout << "difference = " << pLocation3 - pLocation0 << endl;
+
+    cout << "pLocation3 = " << pLocation3 << endl;
+    cout << "pLocation0 = " << pLocation0 << endl;
+}
+
+int main()
+{
+    withNumbers();
+    withStrings();
 }
