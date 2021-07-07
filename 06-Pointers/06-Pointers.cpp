8d2abd6e24985a24129aded60f3a65c067847432
Kieran Everett <kieraneverett03@gmail.com>
2021-07-07 13:52:06 +0100

    Update 06-Pointers.cpp

--- 06-Pointers/06-Pointers.cpp
+++ 06-Pointers/06-Pointers.cpp
@@ -14,12 +14,22 @@ int main()
     cout << c << endl;
 
 
-    int someArray[10];
+    int someArray[10] = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
 
     int* pLocation6 = &someArray[6];
-    int* pLocation0 = &someArray[0];
+    
+    // either of these work because index 0 is the start of the data in RAM of the array
+    int* pLocation0 = &someArray[0]; // this one is a bit clearer though
+    //int* pLocation0 = someArray;
 
     cout << "pLocation6 = " << (int)pLocation6 << endl;
     cout << "pLocation0 = " << (int)pLocation0 << endl;
     cout << "difference = " << pLocation6 - pLocation0 << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << someArray[i] << endl; // outputs the array
+        cout << someArray + i << endl; // outputs the pointer for each location of the array
+        cout << someArray + i << " = " << *(someArray + i) << endl; // gets the value through the pointer
+    }
 }
