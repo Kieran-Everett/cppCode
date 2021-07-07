15ba6894f26c5b716773f5b4f4a8a273ac59af1a
Kieran Everett <kieraneverett03@gmail.com>
2021-07-07 13:38:04 +0100

    Update 06-Pointers.cpp

--- 06-Pointers/06-Pointers.cpp
+++ 06-Pointers/06-Pointers.cpp
@@ -1,20 +1,25 @@
-// 06-Pointers.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a = 10;
+    cout << a << endl;
+
+    int* b;
+    b = &a;
+
+    int c = *b;
+    cout << c << endl;
+
+
+    int someArray[10];
+
+    int* pLocation6 = &someArray[6];
+    int* pLocation0 = &someArray[0];
+
+    cout << "pLocation6 = " << (int)pLocation6 << endl;
+    cout << "pLocation0 = " << (int)pLocation0 << endl;
+    cout << "difference = " << pLocation6 - pLocation0 << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
