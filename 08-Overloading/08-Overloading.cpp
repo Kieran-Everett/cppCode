dddc7cbf0591423a304147521cbb9c61b0b96c4c
Kieran Everett <kieraneverett03@gmail.com>
2021-07-08 23:16:19 +0100

    Update 08-Overloading.cpp

--- 08-Overloading/08-Overloading.cpp
+++ 08-Overloading/08-Overloading.cpp
@@ -1,20 +1,30 @@
-// 08-Overloading.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+class printData
+{
+public:
+    void print(int i)
+    {
+        std::cout << "printing int: " << i << std::endl;
+    }
+    void print(double f)
+    {
+        std::cout << "printing double: " << f << std::endl;
+    }
+    void print(char* c)
+    {
+        std::cout << "printing character: " << c << std::endl;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printData pd;
+
+    pd.print(5);
+
+    pd.print(69.420);
+
+    char testStr[5] = "haah";
+    pd.print(testStr);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
