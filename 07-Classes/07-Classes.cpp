64ad6e87478c869b2bf568b13142d7d51239a2d6
Kieran Everett <kieraneverett03@gmail.com>
2021-07-08 20:59:19 +0100

    Creating class things

--- 07-Classes/07-Classes.cpp
+++ 07-Classes/07-Classes.cpp
@@ -11,6 +11,42 @@ void counter(void)
     std::cout << "count is at: " << i << std::endl;
 }
 
+class Box
+{
+    public:
+        double lenght;
+        double width;
+        double height;
+};
+
+// Base Class
+class Shape
+{
+    public:
+        void setWidth(int w)
+        {
+            width = w;
+        }
+        void setHeight(int h)
+        {
+            height = h;
+        }
+
+    protected:
+        int width;
+        int height;
+};
+
+// Derived Class
+class Rectangle : public Shape
+{
+    public:
+        int getArea()
+        {
+            return (width * height);
+        }
+};
+
 int main()
 {
     count = 5;
@@ -20,6 +56,33 @@ int main()
     {
         counter();
     }
+
+
+    double volume = 0.0;
+
+    Box Box1;
+    Box Box2;
+
+    Box1.height = 4.2;
+    Box1.width = 6.9;
+    Box1.lenght = 7.0;
+
+    Box2.height = 2.0;
+    Box2.width = 52.0;
+    Box2.lenght = 23.0;
+
+    volume = Box1.height * Box1.lenght * Box1.width;
+    std::cout << "Box 1 volume: " << volume << std::endl;
+
+    volume = Box2.height * Box2.lenght * Box2.width;
+    std::cout << "Box 2 volume: " << volume << std::endl;
+
+
+    Rectangle Rect;
+    Rect.setWidth(5);
+    Rect.setHeight(3);
+
+    std::cout << "Area of Rect: " << Rect.getArea() << std::endl;
 
     return 0;
 }
