ffae2555ee0035bb47fab1707451c9c9225315e0
Kieran Everett <kieraneverett03@gmail.com>
2021-07-08 23:28:20 +0100

    Added operator overloading

--- 07-Classes/07-Classes.cpp
+++ 07-Classes/07-Classes.cpp
@@ -56,6 +56,41 @@ class Rectangle : public Shape, public PaintCost
         }
 };
 
+class NewBox
+{
+public:
+    double getVolume(void)
+    {
+        return length * width * height;
+    }
+    void setLength(double len)
+    {
+        length = len;
+    }
+    void setWidth(double wid)
+    {
+        width = wid;
+    }
+    void setHeight(double hei)
+    {
+        height = hei;
+    }
+
+    // Overloading the + operator to add the ability to add boxes together
+    NewBox operator+(const NewBox& b)
+    {
+        NewBox box;
+        box.length = this->length + b.length;
+        box.width = this->width + b.length;
+        box.height = this->height + b.height;
+        return box;
+    }
+private:
+    double length;
+    double width;
+    double height;
+};
+
 int main()
 {
     count = 5;
@@ -95,6 +130,30 @@ int main()
 
     std::cout << "Area of Rect: " << Rect.getArea() << std::endl;
     std::cout << "Cost of paint: " << Rect.getCost(area) << std::endl;
+
+
+    NewBox NewBox1;
+    NewBox NewBox2;
+    NewBox NewBox3;
+    
+    NewBox1.setLength(5.0);
+    NewBox1.setWidth(2.4);
+    NewBox1.setHeight(4.0);
+
+    NewBox2.setLength(2.0);
+    NewBox2.setWidth(5.3);
+    NewBox2.setHeight(1.2);
+
+    volume = NewBox1.getVolume();
+    std::cout << "newbox1 vol: " << volume << std::endl;
+    
+    volume = NewBox2.getVolume();
+    std::cout << "newbox2 vol: " << volume << std::endl;
+
+    NewBox3 = NewBox1 + NewBox2;
+
+    volume = NewBox3.getVolume();
+    std::cout << "newbox3 vol: " << volume << std::endl;
 
     return 0;
 }
