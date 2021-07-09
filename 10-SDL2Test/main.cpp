1b47267c06f8e1099d24acf192120bc8ce369228
Kieran Everett <kieraneverett03@gmail.com>
2021-07-09 19:19:57 +0100

    Setting up SDL and SDL_image init error handling

--- 10-SDL2Test/main.cpp
+++ 10-SDL2Test/main.cpp
@@ -3,7 +3,15 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello World!" << std::endl;
+	// Error handling if SDL doesn't init correctly
+	if (SDL_Init(SDL_INIT_VIDEO) > 0) {
+		std::cout << "SDL_Init has failed. SDL_ERROR: " << SDL_GetError() << std::endl;
+	}
+
+	// Error handling if SDL_image doesn't init correctly
+	if (!(IMG_Init(IMG_INIT_PNG))) {
+		std::cout << "IMG_Init has failed. SDL_ERROR: " << SDL_GetError() << std::endl;
+	}
 
 	return 0;
 }
