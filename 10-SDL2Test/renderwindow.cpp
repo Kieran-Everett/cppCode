2254264e0db2f81fb32c97618671eb52b3e629c9
Kieran Everett <kieraneverett03@gmail.com>
2021-07-09 19:32:46 +0100

    Create renderwindow.cpp

---
+++ 10-SDL2Test/renderwindow.cpp
@@ -0,0 +1,15 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+#include "RenderWindow.hpp"
+
+RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h) : window(NULL), renderer(NULL) {
+	window = SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_SHOWN);
+
+	if (window == NULL) {
+		std::cout << "Window failed to init. SDL_ERROR: " << SDL_GetError() << std::endl;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+}
