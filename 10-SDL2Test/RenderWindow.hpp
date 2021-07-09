e273c4f74723518577431fa2f472cba4feb04ac0
Kieran Everett <kieraneverett03@gmail.com>
2021-07-09 21:26:28 +0100

    Adding texture rendering

--- 10-SDL2Test/RenderWindow.hpp
+++ 10-SDL2Test/RenderWindow.hpp
@@ -5,6 +5,7 @@
 class RenderWindow {
 public:
 	RenderWindow(const char* p_title, int p_w, int p_h); // constructor
+	SDL_Texture* loadTexture(const char* p_filePath);
 	void cleanUp();
 private:
 	SDL_Window* window;
--- 10-SDL2Test/renderwindow.cpp
+++ 10-SDL2Test/renderwindow.cpp
@@ -14,6 +14,17 @@ RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h) : window(NULL)
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
+SDL_Texture* RenderWindow::loadTexture(const char* p_filePath) {
+	SDL_Texture* texture = NULL;
+	texture = IMG_LoadTexture(renderer, p_filePath);
+
+	if (texture == NULL) {
+		std::cout << "Failed to load texture. SDL_ERROR: " << SDL_GetError() << std::endl;
+	}
+
+	return texture;
+}
+
 void RenderWindow::cleanUp() {
 	SDL_DestroyWindow(window);
 }
