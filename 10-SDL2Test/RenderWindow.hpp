24ee4d4adcc554b982acd85621c5982891fce769
Kieran Everett <kieraneverett03@gmail.com>
2021-07-09 19:32:43 +0100

    Create RenderWindow.hpp

---
+++ 10-SDL2Test/RenderWindow.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+
+class RenderWindow {
+public:
+	RenderWindow(const char* p_title, int p_w, int p_h); // constructor
+private:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
